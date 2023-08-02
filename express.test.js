e0b2f15d1eea7b6cad7dda4143b950f9399971e4
tcostello74 <tcostello74@yahoo.com>
2023-08-02 10:21:23 -0400

    Fixes

--- express.test.js
+++ express.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./app');
+const app = require('./express');
 
 describe('Statistics', function() {
     it('should calculate mean', async () => {
